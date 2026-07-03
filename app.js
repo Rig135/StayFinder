2e2bab38de367c6f3240408f4151d9ab19194cae
Rig135 <167835509+Rig135@users.noreply.github.com.>
2026-07-03 19:34:33 +0530

    basic CRUD functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Listing = require('./models/listing');
 
 
@@ -16,19 +17,53 @@ const app = express();
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 
 app.get('/',(req,res)=>{
     res.render('home');
+});
+
+app.get('/listings', async (req,res)=>{
+    const allStays = await Listing.find({});
+    res.render('listings/index', {allStays});
+});
+
+app.post('/listings', async (req,res)=>{
+    const listing = new Listing(req.body.listing);
+    await listing.save();
+    res.redirect(`/listings/${listing._id}`);
 })
 
-app.get('/makelisting',async (req,res)=>{
-    const stay = new Listing({title: 'My Attic',price: '1500', description: 'cheap Stay!'});
-    await stay.save();
-    res.send(stay);
+
+app.get('/listings/new', (req,res)=>{
+    res.render('listings/new');
+});
+
+app.get('/listings/:id', async (req,res)=>{
+    const listing = await Listing.findById(req.params.id);
+    res.render('listings/show',{ listing });
+});
+
+app.get('/listings/:id/edit', async (req,res)=>{
+    const listing = await Listing.findById(req.params.id);
+    res.render('listings/edit',{ listing });
+});
+
+app.put('/listings/:id', async (req,res)=>{
+    const {id} = req.params;
+    const listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${listing._id}`);
+})
+
+app.delete('/listings/:id', async (req,res)=>{
+    const {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect('/listings');
 })
 
 
 app.listen(3000, ()=>{
     console.log("Serving on port 3000");
-})
+});
